972a61a683ee6b0d5eef5f1851923baf6d304f88
JingBh <jingbohao@yeah.net>
2020-10-04 10:32:37 +0800

    Add VSCode debug profile.

--- nuxt.config.js
+++ nuxt.config.js
@@ -105,5 +105,10 @@ export default {
 
   // Build Configuration (https://go.nuxtjs.dev/config-build)
   build: {
+    extend(config, { isDev, isClient }) {
+      if (isDev && isClient) {
+        config.devtool = isClient ? 'source-map' : 'inline-source-map'
+      }
+    }
   }
 }
